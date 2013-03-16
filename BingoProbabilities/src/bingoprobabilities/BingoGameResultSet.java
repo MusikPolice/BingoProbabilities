35c3bd87aa21e6718779ef789492b98345e70420
MusikPolice <jonfritz@gmail.com>
2013-03-16 17:15:21 -0400

    Removed unused standard deviation calculation from bingo result set

--- BingoProbabilities/src/bingoprobabilities/BingoGameResultSet.java
+++ BingoProbabilities/src/bingoprobabilities/BingoGameResultSet.java
@@ -9,13 +9,11 @@ public class BingoGameResultSet {
     private final int numCardsActive;
     private final int numBingosAvailable;
     private double mean = 0;
-    private double standardDeviation = 0;
 
     public BingoGameResultSet(int numCardsActive, int numBingosAvailable, List<Integer> numBallsCalledInEachGame) {
         this.numCardsActive = numCardsActive;
         this.numBingosAvailable = numBingosAvailable;
         this.mean = calculateMean(numBallsCalledInEachGame);
-        this.standardDeviation = calculateStandardDeviation(numBallsCalledInEachGame);
     }
 
     public int getNumCardsActive() {
@@ -29,10 +27,6 @@ public class BingoGameResultSet {
     public double getMeanNumberOfBallsCalled() {
         return mean;
     }
-
-    public double getStandardDeviationOfNumberOfBallsCalled() {
-        return standardDeviation;
-    }
     
     private double calculateMean(List<Integer> data) {
         // mean is just an average
@@ -45,19 +39,5 @@ public class BingoGameResultSet {
             m += data.get(i);
         }
         return m / (double)n;
-    }
-
-    private double calculateStandardDeviation(List<Integer> data) {
-        // standard deviation is sqrt of sum of (values-mean) squared divided by n
-        double m = calculateMean(data);
-
-        // calculate the sum of squares
-        double sum = 0;
-        for (int i = 0; i < data.size(); i++) {
-            final double v = data.get(i) - m;
-            sum += v * v;
-        }
-    
-        return Math.sqrt(sum / data.size());
     }
 }
