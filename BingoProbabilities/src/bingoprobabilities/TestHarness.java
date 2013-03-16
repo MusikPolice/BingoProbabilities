877266439d1565842f842bc5d3e07f174d6846e1
MusikPolice <jonfritz@gmail.com>
2013-03-16 15:08:12 -0400

    Introduced a thread pool to take advantage of all cores of my cpu. Memory use is still a concern

--- BingoProbabilities/src/bingoprobabilities/TestHarness.java
+++ BingoProbabilities/src/bingoprobabilities/TestHarness.java
@@ -6,6 +6,11 @@ import bingo.winconditions.HorizontalLineEvaluator;
 import bingo.winconditions.WinConditionEvaluator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -13,7 +18,7 @@ import java.util.List;
  */
 public final class TestHarness {
 
-    public TestHarness() {
+    public TestHarness() throws InterruptedException {
         int MIN_BINGOS_AVAILABLE = 1;
         int MAX_BINGOS_AVAILABLE = 400;
         int MIN_CARDS_IN_PLAY = 1;
@@ -21,76 +26,93 @@ public final class TestHarness {
         int NUM_GAMES = 100;
 
         long startTime = System.currentTimeMillis();
-        List<BingoGameResultSet> overallResults = new ArrayList<BingoGameResultSet>();
+        
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        List<Future<BingoGameResultSet>> results = new ArrayList<Future<BingoGameResultSet>>();
         for (int bingosAvailable = MIN_BINGOS_AVAILABLE; bingosAvailable <= MAX_BINGOS_AVAILABLE; bingosAvailable++) {
             for (int cardsInPlay = MIN_CARDS_IN_PLAY; cardsInPlay <= MAX_CARDS_IN_PLAY; cardsInPlay++) {
-                BingoGameResultSet result = new BingoGameResultSet(cardsInPlay, bingosAvailable);
-                for (int game = 0; game < NUM_GAMES; game++) {
-                    result.addResult(playBingoGame(cardsInPlay, bingosAvailable));
-                }
-                overallResults.add(result);
-                System.out.println(bingosAvailable + " Bingos Available + " + cardsInPlay + " Cards in Play = " + result.getMeanNumBallsCalled() + " Balls Called.");
+                results.add(threadPool.submit(new BingoGameSimulator(NUM_GAMES, cardsInPlay, bingosAvailable)));
             }
         }
         
-        System.out.println("Gathered " + overallResults.size() + " sample sets over " + (System.currentTimeMillis() - startTime) + "ms");
+        threadPool.awaitTermination(1, TimeUnit.HOURS);
+        System.out.println("Gathered " + results.size() + " sample sets over " + (System.currentTimeMillis() - startTime) + "ms");
     }
 
-    private BingoGameResult playBingoGame(int numCards, int numBingos) {
-        if (numCards < numBingos) {
-            throw new IllegalArgumentException("Not enough cards to get all of the bingos");
+    private class BingoGameSimulator implements Callable<BingoGameResultSet> {
+
+        private final int numGames;
+        private final int numCards;
+        private final int numBingos;
+
+        public BingoGameSimulator(int numGames, int numCards, int numBingos) {
+            this.numGames = numGames;
+            this.numCards = numCards;
+            this.numBingos = numBingos;
         }
 
-        NumberPool balls = new NumberPool(1, 75);
-        WinConditionEvaluator evaluator = new HorizontalLineEvaluator();
-
-        List<Card> cards = new ArrayList<Card>();
-        List<Card> finishedCards = new ArrayList<Card>();
-        int bingos = 0;
-
-        //create some random cards
-        for (int i = 0; i < numCards; i++) {
-            Card c = new Card();
-            cards.add(c);
+        @Override
+        public BingoGameResultSet call() throws Exception {
+            BingoGameResultSet resultSet = new BingoGameResultSet(numCards, numBingos);
+            for (int game = 0; game < numGames; game++) {
+                resultSet.addResult(playBingo());
+            }
+            
+            return resultSet;
         }
+        
+        private BingoGameResult playBingo() {
+            NumberPool balls = new NumberPool(1, 75);
+            WinConditionEvaluator evaluator = new HorizontalLineEvaluator();
 
-        //pull all 75 balls
-        while (balls.hasNext()) {
-            int ball = balls.getNext();
+            List<Card> cards = new ArrayList<Card>();
+            List<Card> finishedCards = new ArrayList<Card>();
+            int bingos = 0;
 
-            //daub each card. if the card has a bingo, remove it from the game
-            for (Card c : cards) {
-                c.daubCell(ball);
+            //create some random cards
+            for (int i = 0; i < numCards; i++) {
+                Card c = new Card();
+                cards.add(c);
+            }
 
-                if (evaluator.hasBingo(c)) {
-                    finishedCards.add(c);
-                    bingos++;
+            //pull all 75 balls
+            while (balls.hasNext()) {
+                int ball = balls.getNext();
+
+                //daub each card. if the card has a bingo, remove it from the game
+                for (Card c : cards) {
+                    c.daubCell(ball);
+
+                    if (evaluator.hasBingo(c)) {
+                        finishedCards.add(c);
+                        bingos++;
+                    }
+                }
+
+                for (Card c : finishedCards) {
+                    cards.remove(c);
+                }
+
+                //if there are no more cards in play, the game is over
+                if (cards.isEmpty()) {
+                    break;
+                }
+
+                //if all of the bingos have been earned, the game is over
+                if (bingos >= numBingos) {
+                    break;
                 }
             }
 
-            for (Card c : finishedCards) {
-                cards.remove(c);
-            }
-
-            //if there are no more cards in play, the game is over
-            if (cards.isEmpty()) {
-                break;
-            }
-
-            //if all of the bingos have been earned, the game is over
-            if (bingos >= numBingos) {
-                break;
-            }
+            //return the game results to caller
+            return new BingoGameResult(Math.min(bingos, numBingos), balls.getNumberOfPulls());
         }
-
-        //return the game results to caller
-        return new BingoGameResult(Math.min(bingos, numBingos), balls.getNumberOfPulls());
     }
-    
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TestHarness t = new TestHarness();
     }
 }
